c4fcd77eaf30cc543c831fc7a902c7ca4fa09d75
nicholas-hoy-champain <nicholas.hoy@mymail.champlain.edu>
2022-10-04 22:18:27 -0400

    Reset Narrative System to clean up assets

--- Assets/Artemis/Examples/Scripts/Systems & Flag Databases/Artemis Narrative System Flag Repo/FlagID.cs
+++ Assets/Artemis/Examples/Scripts/Systems & Flag Databases/Artemis Narrative System Flag Repo/FlagID.cs
@@ -2,20 +2,6 @@ namespace Artemis
 {
 	public enum FlagID
 	{
-		INVALID = -1,
-		CL = 3,
-		CO = 5,
-		FALSEA = 1,
-		FALSEB_1 = 7,
-		FALSEB_2 = 9,
-		FALSEB_3 = 11,
-		FALSEB_4 = 13,
-		OC = 4,
-		OP = 2,
-		TRUEA = 0,
-		TRUEB_1 = 6,
-		TRUEB_2 = 8,
-		TRUEB_3 = 10,
-		TRUEB_4 = 12
+		INVALID = -1
 	}
 }
