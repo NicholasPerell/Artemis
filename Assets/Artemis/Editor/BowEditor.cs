81994ac08cfcdc12fb7759c431c97635fefd80a0
nicholas-hoy-champain <nicholas.hoy@mymail.champlain.edu>
2023-02-04 04:25:01 -0500

    Bow and Fletcher editors removed unused functions for rendering previews

--- Assets/Artemis/Editor/BowEditor.cs
+++ Assets/Artemis/Editor/BowEditor.cs
@@ -26,27 +26,6 @@ namespace Artemis.EditorIntegration
                 Repaint();
             }
         }
-
-        public override Texture2D RenderStaticPreview(string assetPath, Object[] subAssets, int width, int height)
-        {
-            PreDictionaryFletcher example = (PreDictionaryFletcher)target;
-
-            if (example == null)
-                return null;
-
-            Texture2D tex = new Texture2D(width, height);
-            Texture2D copyFrom;
-
-            //TODO: figure out a way to give fletcher coded children a way to have the nice icon.
-
-            //Figure out why Resources works in the Ink Package but not in Artemis
-            //copyFrom = AssetDatabase.Resources<Texture2D>("ArcherFileIcon-Large.png");
-            copyFrom = AssetDatabase.LoadAssetAtPath<Texture2D>("Assets/Artemis/Editor/Resources/bow-arrow.png");
-
-            EditorUtility.CopySerialized(copyFrom, tex);
-
-            return tex;
-        }
     }
 
 }
--- Assets/Artemis/Editor/FletcherEditor.cs
+++ Assets/Artemis/Editor/FletcherEditor.cs
@@ -12,6 +12,10 @@ namespace Artemis.EditorIntegration
         {
             PreDictionaryFletcher e = (PreDictionaryFletcher)target;
 
+            EditorGUIUtility.SetIconForObject(e, AssetDatabase.LoadAssetAtPath<Texture2D>("Assets/Artemis/Editor/Resources/table.png"));
+
+            EditorGUI.BeginChangeCheck();
+
             DrawDefaultInspector();
 
             if (GUILayout.Button("Parse CSV into database"))
@@ -19,36 +23,12 @@ namespace Artemis.EditorIntegration
                 e.DeliverySystemDatabase();
             }
 
-            EditorGUI.BeginChangeCheck();
-
-
             if (EditorGUI.EndChangeCheck())
             {
                 EditorUtility.SetDirty(e);
                 AssetDatabase.SaveAssets();
                 Repaint();
             }
-        }
-
-        public override Texture2D RenderStaticPreview(string assetPath, Object[] subAssets, int width, int height)
-        {
-            PreDictionaryFletcher example = (PreDictionaryFletcher)target;
-
-            if (example == null)
-                return null;
-
-            Texture2D tex = new Texture2D(width, height);
-            Texture2D copyFrom;
-
-            //TODO: figure out a way to give fletcher coded children a way to have the nice icon.
-
-            //Figure out why Resources works in the Ink Package but not in Artemis
-            //copyFrom = AssetDatabase.Resources<Texture2D>("ArcherFileIcon-Large.png");
-            copyFrom = AssetDatabase.LoadAssetAtPath<Texture2D>("Assets/Artemis/Editor/Resources/table.png");
-
-            EditorUtility.CopySerialized(copyFrom, tex);
-
-            return tex;
         }
     }
 
