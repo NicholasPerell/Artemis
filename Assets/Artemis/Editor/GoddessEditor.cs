1869a48f37c2874ae209dadf5570ea16f9c18b0a
nicholas-hoy-champain <nicholas.hoy@mymail.champlain.edu>
2023-02-07 12:42:56 -0500

    Comments added to Goddess editor

--- Assets/Artemis/Editor/GoddessEditor.cs
+++ Assets/Artemis/Editor/GoddessEditor.cs
@@ -21,41 +21,44 @@ namespace Artemis.EditorIntegration
         public override void OnInspectorGUI()
         {
             EditorGUI.BeginChangeCheck();
-            Goddess e = (Goddess)target;
+            Goddess goddess = (Goddess)target;
 
+            //Centered icon for the goddess
             GUILayout.BeginHorizontal();
             int iconSize = 70;
-            GUILayout.Space((EditorGUIUtility.currentViewWidth / 2) - (iconSize/2));
+            GUILayout.Space((EditorGUIUtility.currentViewWidth / 2) - (iconSize/2)); //Space to the left of the icon with the correct sizing to center the icon in the window
             GUILayout.Label(AssetDatabase.LoadAssetAtPath<Texture2D>("Assets/Artemis/Editor/Resources/Goddess.png"), GUILayout.Height(iconSize), GUILayout.Width(iconSize));
             GUILayout.EndHorizontal();
 
             serializedObject.Update();
 
+            //Interactable Properties
             EditorGUILayout.PropertyField(flagsIdsToKeep);
             EditorGUILayout.PropertyField(globallyLoadedFlagBundles);
 
+            //Preview list of all Flag IDs and their value types (SYMBOL, FLOAT, BOOL)
             EditorGUILayout.Space();
-
-            scrollPos = GUILayout.BeginScrollView(scrollPos, false, true);
-            FlagID[] flagIds = e.GetFlagIDs();
+            scrollPos = GUILayout.BeginScrollView(scrollPos, false, true); //Lets the list be scrollable if the list of flags wouldn't normally fit in the window given.
+            FlagID[] flagIds = goddess.GetFlagIDs();
             foreach (FlagID id in flagIds)
             {
-                EditorGUILayout.LabelField(id.ToString(), e.GetFlagValueType(id).ToString());
+                EditorGUILayout.LabelField(id.ToString(), goddess.GetFlagValueType(id).ToString());
             }
             GUILayout.EndScrollView();
 
+            //Reset button
             EditorGUILayout.Space();
             if (GUILayout.Button("RESET \u26A0"))
             {
-                e.Reset();
+                goddess.Reset();
             }
 
             serializedObject.ApplyModifiedProperties();
 
             if (EditorGUI.EndChangeCheck())
             {
-                e.Modify();
-                EditorUtility.SetDirty(e);
+                goddess.Modify();
+                EditorUtility.SetDirty(goddess);
                 AssetDatabase.SaveAssets();
                 Repaint();
             }
--- Assets/Artemis/Editor/GoddessEditorWindow.cs
+++ Assets/Artemis/Editor/GoddessEditorWindow.cs
@@ -7,22 +7,24 @@ namespace Artemis.EditorIntegration
 {
     public class GoddessEditorWindow : EditorWindow
     {
+        Editor editor;
+
         [MenuItem("Window/Artemis Goddess")]
         static void Init()
         {
-            // Get existing open window or if none, make a new one:
-            GoddessEditorWindow window = (GoddessEditorWindow)EditorWindow.GetWindow(typeof(GoddessEditorWindow));
+            //Get first existing window of this type, or (if none) make a new one:
+            GoddessEditorWindow window = (GoddessEditorWindow)GetWindow(typeof(GoddessEditorWindow));
             window.titleContent = new GUIContent("Goddess", AssetDatabase.LoadAssetAtPath<Texture2D>("Assets/Artemis/Editor/Resources/Goddess.png"));
             window.Show();
         }
 
-        Editor editor;
         void OnGUI()
         {
-            editor ??= GoddessEditor.CreateEditor(Goddess.instance);
+            if(editor == null)
+            {
+                editor = GoddessEditor.CreateEditor(Goddess.instance);
+            }
             editor.OnInspectorGUI();
-        }
-
-        
+        }   
     }
 }
