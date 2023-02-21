31e7257c189ea091914c1990274424f77c318d28
nicholas-hoy-champain <nicholas.hoy@mymail.champlain.edu>
2023-02-21 18:18:50 -0500

    UI script for constant pixel size, keeps the item a square shape

---
+++ Assets/Artemis/Examples/Rituals/Scripts/CircularSorterUI.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine;
+
+public class CircularSorterUI : MonoBehaviour
+{
+    [SerializeField]
+    RectTransform[] items;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        RepostionItems();
+    }
+
+    void RepostionItems()
+    {
+
+    }
+}
