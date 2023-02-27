35dd99a06efa27d0033ee1d441aecad82c0ad166
nicholas-hoy-champain <nicholas.hoy@mymail.champlain.edu>
2023-02-27 18:07:53 -0500

    Debug Example Finalized

---
+++ Assets/Artemis/Examples/Debug Console/Scripts/Basic Firing Code/PersistentArcherFireOnTime.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace Artemis.Example
+{
+    public class PersistentArcherFireOnTime : MonoBehaviour
+    {
+        [SerializeField]
+        Archer archer;
+        [Space]
+        [SerializeField]
+        FlagBundle[] importedStates;
+        [SerializeField]
+        FlagID[] all;
+        [Space]
+        [SerializeField]
+        [Range(0.001f, 3.0f)]
+        private float timeBetweenFires = 3.0f;
+        private float timer;
+
+        void Start()
+        {
+            timer = 0;
+        }
+
+        void Update()
+        {
+            if (timer < timeBetweenFires)
+            {
+                timer += Time.deltaTime;
+                if (timer >= timeBetweenFires)
+                {
+                    WhenFiresCodedDebug();
+                    timer = 0;
+                }
+            }
+        }
+
+        void WhenFiresCodedDebug()
+        {
+            archer.AttemptDelivery(importedStates, all);
+        }
+
+    }
+}
