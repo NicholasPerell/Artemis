c9f89eb8a31870fa0597960a82557b6d66a51407
nicholas-hoy-champain <nicholas.hoy@mymail.champlain.edu>
2023-03-09 12:05:21 -0500

    In CorruptionScreenDisplay.cs, maxing out now has an accessible range value for how fast it lerps to the maxed out value

--- Assets/Artemis/Examples/Rituals/Scripts/CorruptionScreenDisplay.cs
+++ Assets/Artemis/Examples/Rituals/Scripts/CorruptionScreenDisplay.cs
@@ -3,69 +3,91 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class CorruptionScreenDisplay : MonoBehaviour
+namespace Artemis.Example.Rituals
 {
-    [SerializeField]
-    Image sliceDisplay;
-
-    [Space]
-    [SerializeField]
-    [Min(0)]
-    float startPixelMultiplier;
-    [SerializeField]
-    [Min(0)]
-    float endPixelMultiplier;
-    
-    [Space]
-    [SerializeField]
-    float checkInterval;
-    [SerializeField]
-    float corruptionChance;
-    [SerializeField]
-    float baseChance;
-    [SerializeField]
-    float corruptionTimeOfCertainty;
-    [SerializeField]
-    float timeFilling;
-    [SerializeField]
-    float baseAmount;
-
-    [Space]
-    [SerializeField]
-    float waveFrequency;
-    [SerializeField]
-    float waveAmplitude;
-
-    // Start is called before the first frame update
-    void Start()
+    public class CorruptionScreenDisplay : MonoBehaviour
     {
-        timeFilling = 0;
-        corruptionChance = baseChance;
-        corruptionTimeOfCertainty = checkInterval / corruptionChance;
-        baseAmount = 0;
-    }
+        [SerializeField]
+        Image sliceDisplay;
 
-    // Update is called once per frame
-    void Update()
-    {
-        timeFilling += Time.deltaTime;
+        [Space]
+        [SerializeField]
+        [Min(0)]
+        float startPixelMultiplier;
+        [SerializeField]
+        [Min(0)]
+        float endPixelMultiplier;
 
-        sliceDisplay.pixelsPerUnitMultiplier = Mathf.Lerp(startPixelMultiplier,endPixelMultiplier,baseAmount + (timeFilling / corruptionTimeOfCertainty * (1 - baseAmount))) + (Mathf.Cos(Time.time * waveFrequency) - .5f) * waveAmplitude;
-    }
+        [Space]
+        [SerializeField]
+        [Min(0.1f)]
+        float checkInterval;
+        [SerializeField]
+        float corruptionChance;
+        [SerializeField]
+        [Min(0.0001f)]
+        float baseChance;
+        [SerializeField]
+        float corruptionTimeOfCertainty;
+        [SerializeField]
+        float timeFilling;
+        [SerializeField]
+        float baseAmount;
 
-    [ContextMenu("ChangeStuffs")]
-    void SetNewCorruptionChance()
-    {
-        float oldTimeLeft = corruptionTimeOfCertainty - timeFilling;
-        float newTimeLeft = checkInterval / corruptionChance;
+        [Space]
+        [SerializeField]
+        float waveFrequency;
+        [SerializeField]
+        float waveAmplitude;
 
-        if (newTimeLeft < oldTimeLeft)
+        [Space]
+        [SerializeField]
+        bool maxOut;
+        [SerializeField]
+        [Range(0 + float.Epsilon, 1)]
+        float lerpToMax = .75f;
+
+        // Start is called before the first frame update
+        void Start()
         {
-            baseAmount = baseAmount + (timeFilling / corruptionTimeOfCertainty * (1 - baseAmount));
-            corruptionTimeOfCertainty = checkInterval / corruptionChance;
+            BeginBuildUp();
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            timeFilling += Time.deltaTime;
+
+            if (!maxOut)
+            {
+                sliceDisplay.pixelsPerUnitMultiplier = Mathf.Lerp(startPixelMultiplier, endPixelMultiplier, baseAmount + (timeFilling / corruptionTimeOfCertainty * (1 - baseAmount))) + Mathf.Cos(Time.time * waveFrequency) * waveAmplitude * .5f;
+            }
+            else
+            {
+                sliceDisplay.pixelsPerUnitMultiplier = Mathf.Lerp(sliceDisplay.pixelsPerUnitMultiplier, endPixelMultiplier - waveAmplitude * .5f, lerpToMax);
+            }
+        }
+
+        [ContextMenu("ChangeStuffs")]
+        void SetNewCorruptionChance()
+        {
+            float oldTimeLeft = corruptionTimeOfCertainty - timeFilling;
+            float newTimeLeft = checkInterval / corruptionChance;
+
+            if (newTimeLeft < oldTimeLeft)
+            {
+                baseAmount = baseAmount + (timeFilling / corruptionTimeOfCertainty * (1 - baseAmount));
+                corruptionTimeOfCertainty = checkInterval / corruptionChance;
+                timeFilling = 0;
+            }
+        }
+
+        void BeginBuildUp()
+        {
             timeFilling = 0;
+            corruptionChance = baseChance;
+            corruptionTimeOfCertainty = checkInterval / corruptionChance;
+            baseAmount = 0;
         }
     }
-
-
 }
