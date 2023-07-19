dcbfa518538fbdf977b99d5a74d6bd7fb468ca0a
Nicholas Perell <nicholasperell@gmail.com>
2023-07-19 17:07:52 -0400

    blob enemy added

--- Rituals/Scripts/Ruins Interactables/EnemyRanged.cs
+++ Rituals/Scripts/Ruins Interactables/EnemyRanged.cs
@@ -4,15 +4,69 @@ using UnityEngine;
 
 public class EnemyRanged : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField]
+    [Min(0.01f)]
+    float minTimePerLoogie = 1;
+    [SerializeField]
+    [Min(0.01f)]
+    float maxTimePerLoogie = 1;
+    [SerializeField]
+    [Min(0.01f)]
+    float timeBeforeFirstLoogie = 1.5f;
+    [SerializeField]
+    GameObject loogiePrefab;
+    [Space]
+
+    [SerializeField]
+    Transform playerTransform;
+
+    [SerializeField]
+    Animator anim;
+
+    float timeUntilNextLoogie;
+
+    private void OnEnable()
     {
-        
+        timeUntilNextLoogie = timeBeforeFirstLoogie;
+
+        if (playerTransform == null)
+        {
+            //TODO: Better way to set the playerTransform
+            playerTransform = GameObject.FindGameObjectWithTag("Player").transform;
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        UpdateFacing();
+        CheckForAttacking();
+    }
+
+    private void CheckForAttacking()
+    {
+        if (timeUntilNextLoogie > 0)
+        {
+            timeUntilNextLoogie -= Time.deltaTime;
+            if(timeUntilNextLoogie <= 0)
+            {
+                SpawnLoogie();
+            }
+        }
+    }
+
+    private void UpdateFacing()
+    {
+        Vector3 facing =playerTransform.position - transform.position;
+
+        anim.SetFloat("FacingX", facing.x);
+        anim.SetFloat("FacingY", facing.z);
+    }
+
+
+    void SpawnLoogie()
+    {
+        timeUntilNextLoogie = Random.Range(minTimePerLoogie, maxTimePerLoogie);
+        Instantiate(loogiePrefab, transform.position, Quaternion.identity).transform.right = -(playerTransform.position - transform.position);
     }
 }
+
